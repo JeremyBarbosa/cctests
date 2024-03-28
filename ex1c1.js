0cce2b42761847a43db90318c8006d11f1c2ac64
Jeremy Barbosa <git@jeremybarbosa.com>
2024-03-29 03:03:28 +0700

    Create ex1c1.js

---
+++ ex1c1.js
@@ -0,0 +1,11 @@
+// Matches any string as long as it contains the words "hello" and "world", regardless of capitalization
+if (!Components.OutputTerminal.hasOutput(/hello,?\s*world[.,!?]?/i)) {
+  return {
+    pass: false,
+    errors: {
+      friendly: "Does your program print the phrase `Hello, World`?",
+      component: "PersistentCodeEditor",
+    },
+  };
+}
+return { pass: true };
