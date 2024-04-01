8c251e2135927eb868da0a9e45683d4cc6424a18
Jeremy Barbosa <git@jeremybarbosa.com>
2024-04-01 20:08:58 +0700

    Create ex2c1.js

---
+++ ex2c1.js
@@ -0,0 +1,20 @@
+// Generic test for if they included the right filename
+if (Components.OutputTerminal.hasError(/No such file/) {
+  return {
+    pass: false,
+    errors: {
+      friendly: 'Did you change name of the file to `numbers.txt` in the supplied ChatGPT command',
+      component: 'PersistentCodeEditor'
+    }
+  };
+}
+if (!Components.OutputTerminal.hasOutput(/212-555-2101\n212-555-2105\n212-555-2100\n212-555-2106/)) {
+  return {
+    pass: false,
+    errors: {
+      friendly: "Does your command print each phone number to separate line?",
+      component: "PersistentCodeEditor",
+    },
+  };
+}
+return { pass: true };
