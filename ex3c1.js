18fa3e240ed53a006a70d40fa1e0ec8aa6baacf2
Jeremy Barbosa <git@jeremybarbosa.com>
2024-04-01 20:12:51 +0700

    Create ex3c1.js

---
+++ ex3c1.js
@@ -0,0 +1,20 @@
+// Generic test for if they included the right filename
+if (Components.OutputTerminal.hasError(/No such file/) {
+  return {
+    pass: false,
+    errors: {
+      friendly: 'Did you change name of the file to `data.md` in the supplied ChatGPT command',
+      component: 'PersistentCodeEditor'
+    }
+  };
+}
+if (!Components.OutputTerminal.hasOutput(/info@buff-it-clean.com\neats@eatherediner.com\nship@boxedtogo.com/)) {
+  return {
+    pass: false,
+    errors: {
+      friendly: "Does your command print each email to separate line?",
+      component: "PersistentCodeEditor",
+    },
+  };
+}
+return { pass: true };
