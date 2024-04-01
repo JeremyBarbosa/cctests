e6add792a55e80233211a0de6e5437f5a4cdd71f
Jeremy Barbosa <git@jeremybarbosa.com>
2024-04-01 20:17:11 +0700

    Create ex4c1.js

---
+++ ex4c1.js
@@ -0,0 +1,10 @@
+if (!Components.OutputTerminal.hasOutput(/(?=.*\bJohn Doe\b)(?=.*\bEmily Johnson\b)(?=.*\bAhmed Khan\b)(?=.*\bMei Ling\b)(?=.*\bAlejandro Ramirez\b)/)) {
+  return {
+    pass: false,
+    errors: {
+      friendly: "Does your command print each of the customers with at least three orders?",
+      component: "PersistentCodeEditor",
+    },
+  };
+}
+return { pass: true };
