8cb051d0db80fc77620ac3ebf40da875f1065b64
Jeremy Barbosa <git@jeremybarbosa.com>
2024-04-15 16:39:21 +0000

    Create ex7c2.js

---
+++ ex7c2.js
@@ -0,0 +1,37 @@
+// Tests for the included example
+if (!Components.OutputTerminal.hasOutput(/(\d+\s)+\d+/)) {
+  return {
+    pass: false,
+    errors: {
+      friendly: "Does your code preserve and print the original example?",
+      component: "PersistentCodeEditor",
+    },
+  };
+}
+return { pass: true };
+
+// Starting Workspace
+def fibonacci(limit):
+    num1, num2 = 0, 1
+    print(num1, end=" ")
+    while num2 <= limit:
+        print(num2, end=" ")
+        num1, num2 = num2, num1 + num2
+fibonacci(100)
+
+// Final Workspace
+package main
+import "fmt"
+
+func fibonacci(limit int) {
+    num1, num2 := 0, 1
+    fmt.Print(num1, " ")
+    for num2 <= limit {
+        fmt.Print(num2, " ")
+        num1, num2 = num2, num1 + num2
+    }
+}
+
+func main() {
+    fibonacci(100)
+}
